225ffc790433702640f3892a48fa4ae059ac01b3
katterincampos <88002663+katterincampos@users.noreply.github.com>
2021-08-27 15:54:33 -0600

    Add files via upload

---
+++ Formulario MDI y secundarios/Formulario MDI y secundarios/Acerca de.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Formulario_MDI_y_secundarios
+{
+    public partial class Acerca_de : Form
+    {
+        public Acerca_de()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Formulario MDI y secundarios/Formulario MDI y secundarios/Formularios de venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Formulario_MDI_y_secundarios
+{
+    public partial class Formularios_de_venta : Form
+    {
+        public Formularios_de_venta()
+        {
+            InitializeComponent();
+        }
+    }
+}
